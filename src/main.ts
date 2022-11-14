fde4b566f81f10c5dff8f1e76c19f720c7030107
楊昀晟(EricYang) <eeric55tw@gmail.com>
2022-11-14 18:06:34 +0800

    feat:i18n install ok

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './styles/app.css';
+import zh from './language/zh-TW.json';
+import en from './language/en-US.json';
+const i18n = createI18n({
+    legacy: false,
+    locale: "zh-TW",
+    fallbackLocale: "zh-TW",
+    messages: {
+      "zh-TW": zh,
+      "en-US": en
+    },
+    globalInjection: true
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
