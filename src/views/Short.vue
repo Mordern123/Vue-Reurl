4529a5cf01c15f8a6530084b07420c9cb4c9f9de
楊昀晟(EricYang) <eeric55tw@gmail.com>
2022-11-21 15:44:03 +0800

    style: footer color

--- src/views/Short.vue
+++ src/views/Short.vue
@@ -1,21 +1,39 @@
-<script lang="ts" setup>
-import { useRoute } from 'vue-router'
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
 import axios from "axios";
 
-const route = useRoute()
-let short=route.params['short']
-
-axios
-    .get(
-      `https://privatutle-bcdlmykzda-de.a.run.app/api/short/${short}`,
-      )
-      .then((res) => {
-        document.location.href = res.data.leadUrl;
-      })
-      .catch(function (error) {
-        // TODO: error page & "not found" page
-        if (error.response.status != 200) {
-          document.location.href = "/";
-        }
-      });
+export default defineComponent({
+  data() {
+    return {
+      short: "",
+    };
+  },
+  methods: {
+    goToShort() {
+      axios
+        .get(
+          `https://privatutle-bcdlmykzda-de.a.run.app/api/short/${this.short}`
+        )
+        .then((res) => {
+          document.location.href = res.data.leadUrl;
+        })
+        .catch(function (error) {
+          // TODO: error page & "not found" page
+          if (error.response.status != 200) {
+            document.location.href = "/";
+          }
+        });
+    },
+  },
+  mounted() {
+    const route = useRoute();
+    this.short = route.params["short"] as any;
+  },
+});
 </script>
