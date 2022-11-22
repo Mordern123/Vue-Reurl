a272d58ec364c298e5461a6b012721953dbaa9cc
楊昀晟(EricYang) <eeric55tw@gmail.com>
2022-11-22 11:18:32 +0800

    feat: qrcode copy send Url ok

--- src/components/urlComponent/UrlViewer.vue
+++ src/components/urlComponent/UrlViewer.vue
@@ -25,14 +25,18 @@
       <button
         class="reurl-btn btn btn-outline-secondary"
         type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#QRCodeModal"
+        title="QRCode"
       >
         <i class="bi bi-qr-code-scan"></i>
       </button>
-      
+
       <button
         class="reurl-btn btn btn-outline-secondary"
         type="button"
-        data-bs-toggle="tooltip" data-bs-placement="top" title="複製網址"
+        data-bs-toggle="tooltip"
+        title="Copy"
         @click="clickCopy()"
       >
         <i class="bi bi-clipboard-check"></i>
@@ -40,9 +44,43 @@
       <button
         class="reurl-btn btn btn-outline-secondary"
         type="button"
+        data-bs-toggle="tooltip"
+        title="Go"
+        @click="clickGoUrl()"
       >
         <i class="bi bi-send-check"></i>
       </button>
+    </div>
+  </div>
+  <div
+    class="modal fade"
+    id="QRCodeModal"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <p style="font-weight: bolder">
+            {{ `QR Code： ${shortUrl}` }}
+          </p>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body d-flex justify-content-center">
+          <qrCode
+            :value="shortUrl"
+            :size="100"
+            level="H"
+            background="#8895af"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,8 +88,12 @@
   <script lang="ts">
 import { defineComponent } from "vue";
 import axios from "axios";
+import qrCode from "qrcode.vue";
 
 export default defineComponent({
+  components: {
+    qrCode,
+  },
   data() {
     return {
       localHref: "",
@@ -79,8 +121,14 @@ export default defineComponent({
           this.shortUrl = `${this.localHref}${res.data.shortUrl}`;
         });
     },
+    clickShowQRC() {
+      console.log(123);
+    },
     clickCopy() {
       navigator.clipboard.writeText(this.shortUrl);
+    },
+    clickGoUrl() {
+      window.open(this.shortUrl);
     },
   },
   mounted() {
